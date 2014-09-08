0019f99abcdac5513bda226c90e5619cd84d6261
Jeffy <yuanjiefeng@gmail.com>
2014-09-08 09:51:21 +0800

    fix bug

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(favicon());
+app.use(favicon(path.join(__dirname, 'public/images/favicon.ico')));
+console.log(path.join(__dirname, 'public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
