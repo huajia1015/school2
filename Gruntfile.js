5fb331cded67c68db8ab183416f29f6be6ca79a2
Jeffy <yuanjiefeng@gmail.com>
2014-09-13 19:30:40 +0800

    optimazation

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        imagemin: {                          // Task
+            dynamic: {                         // Another target
+                files: [
+                    {
+                        expand: true,                  // Enable dynamic expansion
+                        cwd: './public/images/',                   // Src matches are relative to this path
+                        src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+                        dest: './public/images/'                  // Destination path prefix
+                    }
+                ]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.registerTask('default', ['imagemin']);
+};
